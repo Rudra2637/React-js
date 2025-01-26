41c75ed6d87df082c59f5fbf34eeea8e7d232801
Rudra2637 <singhrudra2637@gmail.com>
2025-01-26 19:23:19 +0530

    Changed

--- vite_react/src/App.jsx
+++ vite_react/src/App.jsx
@@ -1,11 +1,14 @@
-
-
-
+import Hello from './index.jsx'
 
 
 function App() {
   return (
+    <>
+    <Hello/>
     <h1>Hello world with vite</h1>
+    
+    </>
+    
   )
 }
 
---
+++ vite_react/src/index.jsx
@@ -0,0 +1,7 @@
+
+
+function Hello(){
+
+    return <h2>Hello how are you and how is your react journey going</h2>
+}
+export default Hello
